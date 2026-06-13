40e1a9e638d9967832b5c4b5c9a89fd526d6c357
Paul-Ciurean <ciureanpaul2505@gmail.com>
2026-06-14 00:28:08 +0100

    added s3 and some providers

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "pic-sharing-app-tf-state"
+    key          = "terraform/pic-sharing-app/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
